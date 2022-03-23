b5df5acdc1bd66a936e29a4e2d371985f7e748cb
Won <fain9301@yahoo.com>
2022-03-23 12:34:47 +0900

    feat: start ftp let

---
+++ src/main/java/kr/co/won/handler/FtpHandler.java
@@ -0,0 +1,38 @@
+package kr.co.won.handler;
+
+import org.apache.ftpserver.ftplet.*;
+
+import java.io.IOException;
+
+public class FtpHandler implements Ftplet {
+
+    @Override
+    public void init(FtpletContext ftpletContext) throws FtpException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public FtpletResult beforeCommand(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return null;
+    }
+
+    @Override
+    public FtpletResult afterCommand(FtpSession session, FtpRequest request, FtpReply reply) throws FtpException, IOException {
+        return null;
+    }
+
+    @Override
+    public FtpletResult onConnect(FtpSession session) throws FtpException, IOException {
+        return null;
+    }
+
+    @Override
+    public FtpletResult onDisconnect(FtpSession session) throws FtpException, IOException {
+        return null;
+    }
+}
--- src/main/java/kr/co/won/handler/FtpLetCustom.java
+++ src/main/java/kr/co/won/handler/FtpLetCustom.java
@@ -1,7 +1,88 @@
 package kr.co.won.handler;
 
-import org.apache.ftpserver.ftplet.DefaultFtplet;
-import org.apache.ftpserver.ftplet.Ftplet;
+import org.apache.ftpserver.ftplet.*;
+
+import java.io.IOException;
 
 public class FtpLetCustom extends DefaultFtplet {
+
+    @Override
+    public FtpletResult onConnect(FtpSession session) throws FtpException, IOException {
+        return super.onConnect(session);
+    }
+
+    @Override
+    public FtpletResult onDisconnect(FtpSession session) throws FtpException, IOException {
+        return super.onDisconnect(session);
+    }
+
+    @Override
+    public FtpletResult onUploadStart(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return super.onUploadStart(session, request);
+    }
+
+    @Override
+    public FtpletResult onUploadEnd(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return super.onUploadEnd(session, request);
+    }
+
+    @Override
+    public FtpletResult onDownloadStart(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return super.onDownloadStart(session, request);
+    }
+
+    @Override
+    public FtpletResult onDownloadEnd(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return super.onDownloadEnd(session, request);
+    }
+
+    @Override
+    public FtpletResult onRmdirStart(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return super.onRmdirStart(session, request);
+    }
+
+    @Override
+    public FtpletResult onRmdirEnd(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return super.onRmdirEnd(session, request);
+    }
+
+    @Override
+    public FtpletResult onMkdirStart(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return super.onMkdirStart(session, request);
+    }
+
+    @Override
+    public FtpletResult onMkdirEnd(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return super.onMkdirEnd(session, request);
+    }
+
+    @Override
+    public FtpletResult onAppendStart(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return super.onAppendStart(session, request);
+    }
+
+    @Override
+    public FtpletResult onAppendEnd(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return super.onAppendEnd(session, request);
+    }
+
+    @Override
+    public FtpletResult onUploadUniqueStart(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return super.onUploadUniqueStart(session, request);
+    }
+
+    @Override
+    public FtpletResult onUploadUniqueEnd(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return super.onUploadUniqueEnd(session, request);
+    }
+
+    @Override
+    public FtpletResult onRenameStart(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return super.onRenameStart(session, request);
+    }
+
+    @Override
+    public FtpletResult onRenameEnd(FtpSession session, FtpRequest request) throws FtpException, IOException {
+        return super.onRenameEnd(session, request);
+    }
 }
